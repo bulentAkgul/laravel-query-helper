782ca6c2347735ead080c0f320b46e252bb27b69
Bulent Akgul <bulentakgul1980@gmail.com>
2023-02-10 13:57:16 +0300

    dependencies changed

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,21 +2,15 @@
 
 namespace Bakgul\LaravelQueryHelper\Tests;
 
-use Bakgul\LaravelDumpServer\Concerns\HasDumper;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Tests\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use HasDumper, LazilyRefreshDatabase;
-
     public function setUp(): void
     {
         parent::setUp();
 
         $this->seed();
-
-        $this->resetDumper();
     }
 
     public function tearDown(): void
